951e16188b8b12205a881e5b04304533d6948834
Tom Dings <tdings66@yahoo.com>
2014-12-12 15:08:37 -0500

    Added CrashPlan to DVM to get git staging area files.

--- init.el
+++ init.el
@@ -1001,3 +1001,11 @@ by Prelude.")
 ;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 ;(setq frame-background-mode 'dark)
 
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
